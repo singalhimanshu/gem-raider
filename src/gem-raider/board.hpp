db4f1d65079b457591dae4f90c35ef3803fb7823
Himanshu <himansingh24@gmail.com>
2021-05-10 12:00:53 +0530

    Refactor: Add RAII stuff to board class

--- src/gem-raider/board.hpp
+++ src/gem-raider/board.hpp
@@ -19,6 +19,21 @@ class Board {
     }
   }
 
+  virtual ~Board() = default;
+
+  Board(const Board &board) = default;
+
+  Board &operator=(const Board &board) = default;
+
+  Board(Board &&other) noexcept : m_tiles(std::move(other.m_tiles)) {}
+
+  Board &operator=(Board &&board) noexcept {
+    if (&board != this) {
+      this->m_tiles = std::move(board.m_tiles);
+    }
+    return *this;
+  }
+
   void fill() noexcept;
   [[nodiscard]] bool draw(SDL_Surface *surface);
   friend std::ostream &operator<<(std::ostream &out, const Board &board);
