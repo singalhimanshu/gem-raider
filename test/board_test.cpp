4488f97482f725eecf9d5dc06c3ddba8d372429b
Himanshu <himansingh24@gmail.com>
2021-06-11 17:03:58 +0530

    Test: add test to check move tile is in bounds
    This test attempts to move the boundary tiles out of bounds, in that
    case the moveTiles function should return false.

--- test/board_test.cpp
+++ test/board_test.cpp
@@ -27,4 +27,32 @@ TEST(board_test, board_boundary_tile_is_of_type_brick_or_gem) {
     EXPECT_TRUE(tile_type == Type::brick || tile_type == Type::goal);
   }
 }
+
+TEST(board_test, test_move_tile_out_of_bounds) {
+  Board board;
+  board.init();
+  board.fill();
+  // first row
+  for (int c = 0; c < tile::cols; c++) {
+    ASSERT_FALSE(board.moveTile(0, c, Direction::up))
+        << "tile at row: 0, col: " << c << " should not be moved\n";
+  }
+  // first col
+  for (int r = 0; r < tile::rows; r++) {
+    ASSERT_FALSE(board.moveTile(r, 0, Direction::left))
+        << "tile at row: " << r << "col: 0 should not be moved\n";
+  }
+  // last row
+  for (int c = 0; c < tile::cols; c++) {
+    ASSERT_FALSE(board.moveTile(tile::rows - 1, c, Direction::down))
+        << "tile at row: " << tile::rows - 1 << ", col: " << c
+        << " should not be moved\n";
+  }
+  // last col
+  for (int r = 0; r < tile::rows; r++) {
+    ASSERT_FALSE(board.moveTile(r, tile::cols - 1, Direction::right))
+        << "tile at row: " << r << ", col: " << tile::cols - 1
+        << " should not be moved\n";
+  }
+}
 }  // namespace gem_raider
