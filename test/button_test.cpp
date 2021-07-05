a8ea8e073b10bc8c3b938cb2b228213b5af5d418
Himanshu <himansingh24@gmail.com>
2021-07-05 22:05:18 +0530

    Test: Remove unused file sdl_util_test
    For now we just had a single function called getTestButton so it didn't
    make much sense to put it inside a different file. Maybe later when
    there are more functions used in different test we can factor these out.
    
    This commit should not change behavior.

--- test/button_test.cpp
+++ test/button_test.cpp
@@ -1,11 +1,18 @@
 #include <gtest/gtest.h>
 
 #include <gem-raider/button.hpp>
-#include <sdl_util_test.cpp>
 #include <string>
 
 namespace gem_raider {
 namespace {
+Button getTestButton(int x_pos, int y_pos, int width, int height) {
+  std::string text = "TEST BUTTON";
+  SDL_Color fg_color{.r = 255, .g = 255, .b = 255, .a = 255};
+  SDL_Color bg_color{.r = 0, .g = 0, .b = 0, .a = 255};
+  return Button(x_pos, y_pos, width, height, text, std::move(fg_color),
+                std::move(bg_color));
+}
+
 TEST(test_button, test_button_click) {
   {
     int x_pos = 0, y_pos = 0;
